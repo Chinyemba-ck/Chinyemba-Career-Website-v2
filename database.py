7d2de60e5cd21f7011beeada22b8eb6cb3a199b8
Chinyemba-ck <85675608+Chinyemba-ck@users.noreply.github.com>
2023-04-10 20:45:44 +0000

    Added a database
    Added a database

---
+++ database.py
@@ -0,0 +1,5 @@
+from sqlalchemy import create_engine,text
+
+engine = create_engine("mysql+pymysql://qg2tx2ijpig1bwa6gu6a:pscale_pw_Ow4hUvsaf6SZzDhhvjOidoYOK5YAqzCZLEsL68gMtbW@aws.connect.psdb.cloud/chinyemba?charset=utf8mb4")
+
+
