f7e57197e8eaf4e5d728aca15bcda4f2fef81e2f
Chinyemba-ck <85675608+Chinyemba-ck@users.noreply.github.com>
2023-04-11 06:01:27 +0000

    Adjusted database

--- database.py
+++ database.py
@@ -1,5 +1,11 @@
 from sqlalchemy import create_engine,text
 
-engine = create_engine("mysql+pymysql://qg2tx2ijpig1bwa6gu6a:pscale_pw_Ow4hUvsaf6SZzDhhvjOidoYOK5YAqzCZLEsL68gMtbW@aws.connect.psdb.cloud/chinyemba?charset=utf8mb4")
+db_connection_string = "mysql+pymysql://qg2tx2ijpig1bwa6gu6a:pscale_pw_Ow4hUvsaf6SZzDhhvjOidoYOK5YAqzCZLEsL68gMtbW@aws.connect.psdb.cloud/chinyemba?charset=utf8mb4" 
+
+engine = create_engine(db_connection_string,    connect_args={
+        "ssl": {
+            "ca": "/etc/ssl/cert.pem"
+        }
+    })
 
 
